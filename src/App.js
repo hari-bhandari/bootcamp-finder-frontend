58968dca1b278d77caeb0c37d32d00e0f1ff77fa
2012b <2012bhandari.ha@gmail.com>
2020-05-10 07:24:25 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
-
+import React,{Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import AuthState from './context/AuthState'
 import './App.css';
+import Navbar from './Components/layouts/Navbar'
+import Home from './Components/pages/Home'
+import Login from './Components/pages/Login'
 
 function App() {
   return (
-    <div className="App">
-      <h1>J</h1>
-    </div>
+    <AuthState>
+    <Router>
+            <Fragment>
+                <Navbar/>
+                <Switch>
+                  <Route exact path={'/'} component={Home}/>
+                  <Route exact path={'/login'} component={Login}/>
+                </Switch>
+            </Fragment>
+        </Router>
+        </AuthState>
   );
 }
 
